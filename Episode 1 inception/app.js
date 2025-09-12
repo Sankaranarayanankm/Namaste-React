40fcd5169174cad08b58fd31e8229b6c4e3c280e
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2025-09-12 07:28:18 +0530

    episode 3

--- Episode 1 inception/app.js
+++ Episode 1 inception/app.js
@@ -1,15 +1,11 @@
-const heading = React.createElement("h1", {}, "Hello World!");
-const child = React.createElement("div", { id: "child" }, heading);
-const parent = React.createElement("div", { id: "parent" }, [child]);
-const render = React.createElement("React.Fragment", {}, [parent, parent]);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(render);
 
-// const heading = React.createElement("h1", {}, "Hello World!");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+const jsxHeading = <h1>Namaste React using JSX</h1>;
+console.log(jsxHeading);
+console.log(heading);
 
-// console.log(root);
-// console.log(heading);
- 
+root.render(jsxHeading);
